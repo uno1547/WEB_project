f46044513b0b9da7317b70d44ae41e893f3eb958
uno1547 <63580165+uno1547@users.noreply.github.com>
2020-04-17 21:03:04 +0900

    JS extract emptycell

--- tictactoe.js
+++ tictactoe.js
@@ -4,53 +4,77 @@ document.addEventListener("DOMContentLoaded",function(){//페이지로드후에 
         each_button[i].addEventListener("click",whenclick)
     }
 })
-function whenclick(){//whenclick 함수
-    if(this.className=='Blue'){
+function whenclick(){//whenclick 함수 gamestart 실행
+    if(this.className =='Blue'){//누른버튼이 블루
         var yourColor = 'Blue'
-        var AIcolor = 'Red'
-    }else{
+        var AIcolor = 'Red'                                //랜덤으로 두기
+    }else{//누른 버튼이 레드
         var yourColor = 'Red'
         var AIcolor = 'Blue'
     }
     var trash = document.querySelector('.button')
-    var TF = true
     trash.remove()
-    console.log(yourColor)
-    console.log(AIcolor)
-    gamestart(yourColor,AIcolor,TF)
+    gamestart(yourColor,AIcolor)
 }
-function gamestart(x,y,z){
-    if(x=='Blue' && z == true){
-            client(x)
-            //AI(y)
-            //judge()
+/*function whetherput(){///////////////////////////////////
+    whetherclicked = true////////////////////////////
+    console.log(whetherclicked)//////////////////
+    console.log('clicked')
+    ////////////////////////
+}*/
+function whetherput(TF){
+    console.log('emptycellready')
+    var empty_list = []
+    if(TF == true){
+        console.log('True!!')
+        var cells = document.querySelectorAll('td')
+        for(i=0;i<9;i++){
+            console.log(cells[i])
+            if(cells[i].textContent == 'O'){//만약 채워진칸이다
+                console.log(cells[i])
+            }else{//나머지 안채워진칸들
+                empty_list.push(cells[i])
+            }
+        }
+        console.log(empty_list)
     }else{
-        client(x)
-        //AI(y)
-        //judge()
-        }dfkfjdkf
+        //pass
     }
-helloguys
-function client(cl){//클라이언트 
-    var click = false
-    var td = document.querySelectorAll('td')
+}
+function gamestart(x,y){//색깔받고 게임스타트
+    client(x,y)
+    /*if(whetherclicked == true){
+        console.log('clicked')
+    }else{
+        console.log()
+    }*/   
+}
+function AI(aicl){
+    console.log('AIcoloris'+aicl)
+}
+function GetEmptyCell(){
+    cells = document.querySelectorAll('td')
+
+}
+function client(cl,aicl){//옌그냥 이벤트 리스너등록까지만하고끝
+    var td = document.querySelectorAll('td');
+    click = false
     if(cl == 'Blue'){
         for(i=0;i<td.length;i++){
             td[i].addEventListener("mouseover",blueshadowon)
             td[i].addEventListener("mouseout",shadowoff)
             td[i].addEventListener("click",blueput)
-
         }
     }else{
         for(i=0;i<td.length;i++){
             td[i].addEventListener("mouseover",redshadowon)
             td[i].addEventListener("mouseout",shadowoff)
             td[i].addEventListener("click",redput)
-    }
-}
-function AI(cl){
-    
-    
+            }
+        }    
+    /*if(clicked){
+        AI(aicl)
+    }*/
 }
 function blueshadowon(){
     if(click == false){
@@ -60,11 +84,6 @@ function blueshadowon(){
         //pass
     }
 }
-function blueput(){
-    this.style.color = 'Blue'
-    this.innerHTML = 'O'
-    click = true
-}
 function redshadowon(){
     if(click == false){
     this.style.color = 'Salmon'
@@ -73,212 +92,49 @@ function redshadowon(){
         //pass
     }
 }
-function redput(){
-    this.style.color ='Red'
-    this.innerHTML = 'X'
-    click= true
-}
-function shadowoff(){
-    if(click == false){
-        this.innerHTML = ''
-    }else{
-        //pass
-    }
-}
-/*function shadowon(x){
-    if(x=='Blue'){
-        this.style.color = 'DeepSkyBlue'
-        this.innerHTML = 'O'
-    }else{
-        this.style.color = 'Salmon'
-        this.innerHTML = 'X'
-    }
-}
-function put(x){
-    if(x=='Blue'){
-        this.style.color = x
-        this.innerHTML = 'O'
-    }else{
-        this.style.color = x
-        this.innerHTML = 'X'
-    }
-}
-*/
-/*
-function blueshadowon(){
-    if(click == false){
-    this.style.color = 'DeepSkyBlue'
-    this.innerHTML = 'O'
-    }else{
-        //pass
-    }
-}
 function blueput(){
-    this.style.color = 'Blue'
-    this.innerHTML = 'O'
-    click = true
-}
-function redshadowon(){
-    if(click==false){
-    this.style.color = 'Salmon'
-    this.innerHTML = 'X'
+    if(click == false){
+        this.style.color = 'Blue'
+        this.innerHTML = 'O'
+        click = true
+        var whetherclick = true
+        console.log(whetherclick)//true 출력 근데 왜 wheterput에 true가전달이안돼ㅠㅠㅠㅠㅠ
+        whetherput(whetherclick)//안됨
+        console.log('blueput')
+        AI('Red')//////////
+        
+        /*if(this.length == 0 ){
+            console.log('it says 0')
+        }else{
+            console.log('it says no 0')
+            a = this.length
+            target = document.querySelector('.box')
+            target.innerHTML = a
+        }
+        */
     }else{
         //pass
     }
+
 }
 function redput(){
-    this.style.color ='Red'
-    this.innerHTML = 'X'
-    click= true
+    if(click == false){//클릭하면?
+        this.style.color ='Red'
+        this.innerHTML = 'X'
+        click = true
+        whetherput()
+        console.log('redput')
+        AI('Blue')//////////
+    }else{
+        //pass
+    }
+
 }
 function shadowoff(){
     if(click == false){
         this.innerHTML = ''
+        this.style.color = ''
     }else{
         //pass
     }
 }
-/*
-function gamestart(x){
-    if(x=='Blue'){
-        client('Blue')
-        console.log('blueactivateds')
-        console.log(x)
-    }else{
-        client('Red')
-        console.log(x)
-    }
-}
-        /*console.log(each_button)
-    for (i=0;i<each_button.length;i++){//2개버튼에 줌
-        each_button[i].addEventListener("click",whenclick)
-        console.log(each_button[i])
-        console.log(i)
-    }
-    //var yourColor = whenclick()
-})
-
-/*function shadowon(evt){
-    if(el==true){
-        evt.target.style.color = 'deepskyblue';
-        evt.target.innerHTML = 'O'
-    }else{
-        //pass
-    }
-}
-function shadowoff(evt){
-    if(el==true){
-        evt.target.innerHTML = ''
-    }else{
-        //pass
-    }
-}
-function put(evt){
-    if(el = true){
-        evt.target.style.color = 'blue';
-        evt.target.innerHTML = 'O';
-        click = true;
-    }else{
-        //pass
-    }
-}
-*/
-/*document.addEventListener("DOMContentLoaded" , function(){
-    var target = document.querySelectorAll('td')
-    var i;
-    for (i= 0;i<9;i++){
-        target[i].addEventListener("mouseover",shadowon)
-        target[i].addEventListener("mouseout",shadowoff)
-        target[i].addEventListener("click",put)
-    }
-})
-*/
-
-/*function shadownon(evt){
-    if(click == false){
-        evt.cells.style.color = 'deepskyblue';
-        evt.cells.innerHTML = 'O'
-        console.log(cells[i])
-    }else{
-        //pass
-    }
-}
-function shadowoff(evt){
-    if(click == false){
-        evt.cells.innerHTML = ''
-    }
-}
-function put(evt){
-    evt.cells.style.color = 'blue'
-    evt.cells.innerHTML = 'O'
-    click = true
-}
-function client(cl){
-    var click = false
-    var td = document.querySelectorAll('td')
-    if(cl == 'Blue'){//컬러로 블루받았을때 클라이언트
-        console.log(cl)//블루받을때 출력
-        for(i=0;i<td.length;i++){
-            td[i].addEventListener("click",function(){
-                td[i].style.color = 'blue'
-            })
-        }
-    }else{
-        for(i=0;i<td.length;i++){
-            td[i].addEventListener("mouseover",function(){//커서올리면 클릭안됬을때
-                if(click == false){
-                    td[i].style.color = 'Salmon'
-                    td[i].innerHTML = 'X'
-                }else{
-                    //pass
-                }
-            })
-            td[i].addEventListener("mouseout",function(){
-                if(click == false){
-                    td[i].innerHTML =''
-                }else{
-                    //pass
-                }
-            })
-            td[i].addEventListener("click",function(){//클릭
-                td[i].style.color = cl
-                td[i].innerHTML = 'X'
-                click = true
-            })
-        }
-    }
-    
-}
-function gamestart(x){
-    if(x=='Blue'){
-        client('Blue')
-        console.log('blueactivateds')
-        console.log(x)
-    }else{
-        client('Red')
-        console.log(x)
-    }
-}
-function whenclick(){
-    if(this.className =='Blue'){
-        var yourColor = 'Blue'
-        console.log(yourColor)//블루면 블루 출력
-    }else{
-        var yourColor = 'Red'
-        console.log(yourColor)
-    }
-    var trash = document.querySelector('.button');
-    console.log(yourColor)//색깔 두번째 출력
-    trash.remove();
-    gamestart(yourColor)//색깔 인자로 넘김
-}
-document.addEventListener("DOMContentLoaded", function(){//여긴문제없는듯
-    var tdtag = document.querySelectorAll('td')
-    for (i=0;i<tdtag.length;i++){
-        tdtag[i].addEventListener("mouseover",function(){
-            tdtag[i].style.color = 'blue'
-            tdtag[i].innerHTML = 'O'
-        })
-    }
-}
-*/}
