15161ad2a6598e1eb724da881dc5cbd01aa8804b
uno1547 <63580165+uno1547@users.noreply.github.com>
2020-04-28 23:54:03 +0900

    only ai left!!

--- tictactoe2.js
+++ tictactoe2.js
@@ -3,18 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     for(q = 0; q < each_button.length; q++) {
         each_button[q].addEventListener("click", Whenclick)
     }
-    console.log(arraycontainsarray([3,4,6,7,8],[6,7,8]))
 })
 
-function Whenclick() {//Whenclick 함수 gamestart 실행
-    if(this.className == 'Blue') {//누른버튼이 블루
+function Whenclick() {
+    if(this.className == 'Blue') {
         var YourColor = 'Blue'
-        console.log('yourchoiceisblue')
-        //var AIcolor = 'Red'                                //랜덤으로 두기
     } else {//누른 버튼이 레드
         var YourColor = 'Red'
-        console.log('yourchoiceisred')
-        //var AIcolor = 'Blue'
     }
     var trash = document.querySelector('.button')
     trash.remove()
@@ -22,17 +17,17 @@ function Whenclick() {//Whenclick 함수 gamestart 실행
 }
 
 function clientturn(clientcolor) {
-    console.log('client turn!') //옌그냥 이벤트 리스너등록까지만하고끝 등록했던 redblueput에서 whetherput 중복추가
     var td = document.querySelectorAll('td');
     var sparecell = []
-    console.log(sparecell)
-    for(x=0; x<9; x++){
-        if(td[x].innerHTML == ''){
+    for(x = 0; x < td.length; x++) {
+        if(td[x].className != 'clicked') {
             sparecell.push(td[x])
+            
         }else{
-            //pass
+        //pass
         }
     }
+    console.log(sparecell)
     if(clientcolor == 'Blue') {
         for(i=0; i<sparecell.length; i++) {
             sparecell[i].addEventListener("mouseover", blueshadowon)
@@ -49,61 +44,71 @@ function clientturn(clientcolor) {
 }
 
 function blueshadowon() {
-    this.style.color = 'DeepSkyBlue'
-    this.innerHTML = 'O'
+    if(this.className != 'clicked') {
+        this.style.color = 'DeepSkyBlue'
+        this.innerHTML = 'O'
+    }else{
+        //pass
+    }
 }
 
 function redshadowon() {
-    this.style.color = 'Salmon'
-    this.innerHTML = 'X'
+    if(this.className != 'clicked') {
+        this.style.color = 'salmon'
+        this.innerHTML = 'X'
+    }
 }
 
 function blueput() {
+    if(this.className != 'clicked') {
         this.style.color = 'Blue'
         this.innerHTML = 'O'
-        click = true
-        console.log('blueputactivated')
-        judge('AI', 'Red')//안됨
+        this.className = 'clicked'
+        judge('AI', 'Red')
+    }
 }
 
-function redput() {//클릭하면?
+function redput() {
+    if(this.className != 'clicked') {
         this.style.color = 'Red'
         this.innerHTML = 'X'
-        click = true
-        console.log('redputactivated')
+        this.className = 'clicked'
         judge('AI', 'Blue')
+    }
 }
 
 function shadowoff() {
+    if(this.className != 'clicked') {
         this.innerHTML = ''
         this.style.color = ''
+    }else{
+        //pass
+    }
 }
 
 function AIturn(AIcolor){
-    console.log('ai turn!')
     var empty_list = []
     var cells = document.querySelectorAll('td')
     for(k = 0; k <9; k++) {
-        //console.log(cells[i])
-        if(cells[k].textContent != '') {//만약 채워진칸이다 
-            //console.log(cells[k])
+        ////console.log(cells[i])
+        if(cells[k].textContent != '') {
+            ////console.log(cells[k])
         } else {//나머지 안채워진칸들
             empty_list.push(cells[k])
         }
     }
     var randomcell = empty_list[Math.floor(Math.random()* empty_list.length)]
-    //console.log(randomcell)
-    //console.log('yourrandomcell')
     if(AIcolor == 'Red') {
         randomcell.style.color = AIcolor
         randomcell.innerHTML = 'X'
+        randomcell.className = 'clicked'
         judge('client', 'Blue')
     } else {
         randomcell.style.color = AIcolor
         randomcell.innerHTML = 'O'
+        randomcell.className = 'clicked'
         judge('client', 'Red')
     }
-    
 }
 
 function arraycontainsarray(target, wincase) {
@@ -113,7 +118,6 @@ function arraycontainsarray(target, wincase) {
 }
 
 function judge(turn, color) {
-    console.log('judgeactivated')
     var currenttable = document.querySelectorAll('td')
     var totalcellnumber = currenttable.length
     var winarray = [
@@ -122,50 +126,43 @@ function judge(turn, color) {
     var orray = []
     var xrray = []
     var emptylist = []
-    //console.log(totalcellnumber)
     for(celnum = 0; celnum < totalcellnumber; celnum++) {
         if(currenttable[celnum].innerHTML == 'O') {
-            //console.log(celnum)
             orray.push(celnum)
         } else if (currenttable[celnum].innerHTML == 'X') {
-            //console.log(celnum)
             xrray.push(celnum)
         } else {
             emptylist.push(i)
         }
-    }//셀별로 분류
+    }
     for(z = 0; z < winarray.length; z++) {//승부나는경우 판단
         var bluewin = arraycontainsarray(orray, winarray[z])
         var redwin = arraycontainsarray(xrray, winarray[z])
-        console.log(orray, winarray[z])
-        console.log(xrray, winarray[z])
-        console.log(bluewin, redwin)
+        //console.log(orray, winarray[z])
+        //console.log(xrray, winarray[z])
+        //console.log(bluewin, redwin)
         if(bluewin == true || redwin == true) {
             var status = 'gameover'
-            console.log('gameover')
         }else{
-            console.log('notgameover')
+            //pass
         }
     }
     if(xrray.length + orray.length == 9){
         var status = 'draw'
     }else{
-        console.log('notdraw')
+        //pass
     }
 
     if(status == 'gameover') {
-        console.log('status gameover')
         gameover(orray, xrray)
-    }else if(status == 'draw'){
-        console.log('status draw')
+    }else if(status == 'draw') {
         draw()
     }else{
-        console.log('nextturn!!!')
         nextturn(turn, color)
     }
 }
 
-function gameover(O, X){
+function gameover(O, X) {
     var board = document.querySelector('.gamestatus')
     if(O.length > X.length){
         board.style.display = 'block'
@@ -174,9 +171,13 @@ function gameover(O, X){
         board.style.display = 'block'
         board.innerHTML = 'Redwin'
     }
+    var allcell = document.querySelectorAll('td')
+    for(c = 0; c < allcell.length; c++){
+        allcell[c].className = 'clicked'
+    }
 }
 
-function draw(){
+function draw() {
     var drawboard = document.querySelector('.gamestatus')
     drawboard.style.display = 'block'
     drawboard.innerHTML = 'Draw'
@@ -190,7 +191,3 @@ function nextturn(next, color){
     }
 }
 
-/*xrray.sort(function(c, d){
-   굳이 필요없을듯 포함관계여서 첨자들이 양수라는것만중요순서는상관 없음             
-})
-*/
